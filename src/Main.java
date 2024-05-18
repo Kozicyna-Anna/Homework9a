41050c0d955769bd61b90223ae752fcc484b151b
New <Dar1985@bk.ru>
2024-05-19 00:05:20 +0700

    ДЗ исправлено

--- src/Main.java
+++ src/Main.java
@@ -25,32 +25,32 @@ public class Main {
                 if (element[i] > max) {
                     max = element[i];
                 }
-
-                System.out.println("Минимальная сумма трат за неделю составила " + min + " рублей. " +
-                        "Максимально сумма трат за неделю составила " + max + " рублей");
-                System.out.println();
             }
+            System.out.println("Минимальная сумма трат за неделю составила " + min + " рублей. " +
+                    "Максимально сумма трат за неделю составила " + max + " рублей");
+            System.out.println();
+
         }
-            {
-                System.out.println("\nTask3");
+        {
+            System.out.println("\nTask3");
 
-                int[] element = {562, 9653, 852674, 54721, 659214};
-                int sum = 0;
-                for (int i = 0; i < element.length; i++) {
-                    sum += element[i];
-                }
-                double avg = sum / 4d;
-                System.out.println("Средняя сумма трат за месяц составила " + avg + " рублей");
+            int[] element = {562, 9653, 852674, 54721, 659214};
+            int sum = 0;
+            for (int i = 0; i < element.length; i++) {
+                sum += element[i];
             }
-            {
-                System.out.println("\nTask4");
+            double avg = sum / element.length;
+            System.out.println("Средняя сумма трат за месяц составила " + avg + " рублей");
+        }
+        {
+            System.out.println("\nTask4");
 
-                char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-                for (int i = reverseFullName.length - 1; i >= 0; i--) {
-                    System.out.print(reverseFullName[i]);
-                }
-                System.out.println();
+            char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+            for (int i = reverseFullName.length - 1; i >= 0; i--) {
+                System.out.print(reverseFullName[i]);
             }
+            System.out.println();
+        }
 
     }
 }
